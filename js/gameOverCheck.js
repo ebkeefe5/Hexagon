865bd0e71e05ab98adcc85d2a10d561595b42f5d
unknown <ebkeefe5@gmail.com>
2022-12-31 20:02:48 -0800

    simplify algorithm a little and add note to add in back tracking

--- js/gameOverCheck.js
+++ js/gameOverCheck.js
@@ -1,35 +1,34 @@
-var winningPlayer1Board = null; 
+var visitedRedBoard = null; 
 var size;
+//TODO keep track of the parent of each node and use a back traking algorithm to isolate the path 
 
-//check for a winning path for player 1 and mark the path with 3s if so
 function checkWinBoardPlayer1(board)
 {
-	winningPlayer1Board = JSON.parse(JSON.stringify(board)); 
-	size = winningPlayer1Board.length;
+	visitedRedBoard = JSON.parse(JSON.stringify(board)); 
+	size = visitedRedBoard.length;
 
-	for (var col = 0; col < winningPlayer1Board.length; col++)
+	for (var col = 0; col < visitedRedBoard.length; col++)
 	{
-		if (winningPlayer1Board[0][col] == 1 && checkWinPlayer1(0, col))
-			return winningPlayer1Board;
+		if (visitedRedBoard[0][col] == 1 && checkWinPlayer1(0, col))
+			return visitedRedBoard;
 	}
 
 	return null;
 }
 
-
 function checkWinPlayer1(row, col)
 {
-	if (row < 0 || row >= size || col < 0 || col >= size || winningPlayer1Board[row][col] != 1)
+	if (row < 0 || row >= size || col < 0 || col >= size || visitedRedBoard[row][col] != 1)
 		return false;
 
-	winningPlayer1Board[row][col] = 3;
+	visitedRedBoard[row][col] = 3;
 
 	if (row == size - 1)
 		return true;
 
-	return checkWinPlayer1(row - 1, col - 1) || checkWinPlayer1(row - 1, col) || checkWinPlayer1(row - 1, col + 1)
-	|| checkWinPlayer1(row, col - 1) || checkWinPlayer1(row, col + 1)
-	|| checkWinPlayer1(row + 1, col - 1) || checkWinPlayer1(row  + 1, col) || checkWinPlayer1(row + 1, col + 1);
-
-	winningPlayer1Board[row][col] = 1;
+	const directions = [[-1, -1], [-1, -0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+    for (const [di, dj] of directions) {
+    	if (checkWinPlayer1(row + di, col + dj))
+      		return true;
+  	}
 }
