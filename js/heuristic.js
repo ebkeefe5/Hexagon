3d41690b0a3426f424210300f4d445e4db855b5f
Eric Keefe <ekeefe@clearwateranalytics.com>
2023-05-04 10:16:40 -0600

    add notes for future improvements

--- js/heuristic.js
+++ js/heuristic.js
@@ -1,32 +1,105 @@
-//heuristic to estimate how good board state is for each player 
-//#moves needed for red to win - #moves needed for blue to win
-//red wants to minimize this number, blue wants to maximize this number
+//calculate moves for each player to win but no need to remember path
+
+var size;
+
 function calculateRedMovesToWin(board)
 {
-	var redShortestPath = getRedShortestPath(board);
-	return getHeuristicScore(redShortestPath, board);
+	var trackBoard = JSON.parse(JSON.stringify(board)); //indexed as [row][col] or [yPos][xPos]
+    var toVisit = new PriorityQueue(); //min heap of hexagons to explore
+    size = trackBoard.length;
+	for (var col = 0; col < trackBoard.length; col++)
+	{
+		insertHexagonRed(col, 0, trackBoard[0][col], 0, toVisit, trackBoard);
+	}
+
+	const directions = [[0, -1], [1, -1],
+						[-1, 0], [1, 0],
+						[-1, 1], [0, 1]];
+
+	while(!toVisit.isEmpty())
+	{
+		var hex = toVisit.pop();
+		if (hex.yPos == size - 1)
+			return hex.stepsFromStart;
+		else
+		{
+			for (const [di, dj] of directions)
+			{
+				var col = hex.xPos + di; var row = hex.yPos + dj;
+		    	if (row >= 0 && row < size && col >= 0 && col < size)
+		    	{
+		    		insertHexagonRed(col, row, trackBoard[row][col], hex.stepsFromStart, toVisit, trackBoard);
+		    	}
+	    	}
+		}
+	}
+	console.log("error: trying to check min moves to win for red and the game is over");
+	return 100000;
 }
 
 function calculateBlueMovesToWin(board)
 {
-	var blueShortestPath = getBlueShortestPath(board);
-	return getHeuristicScore(blueShortestPath, board);
+	var trackBoard = JSON.parse(JSON.stringify(board)); //indexed as [row][col] or [yPos][xPos]
+    var toVisit = new PriorityQueue(); //min heap of hexagons to explore
+    size = trackBoard.length;
+	for (var row = 0; row < trackBoard.length; row++)
+	{
+		insertHexagonBlue(0, row, trackBoard[row][0], 0, toVisit, trackBoard);
+	}
+
+	const directions = [[0, -1], [1, -1],
+						[-1, 0], [1, 0],
+						[-1, 1], [0, 1]];
+
+	while(!toVisit.isEmpty())
+	{
+		var hex = toVisit.pop();
+		if (hex.xPos == size - 1)
+			return hex.stepsFromStart;
+		else
+		{
+			for (const [di, dj] of directions)
+			{
+				var col = hex.xPos + di; var row = hex.yPos + dj;
+		    	if (row >= 0 && row < size && col >= 0 && col < size)
+		    	{
+		    		insertHexagonBlue(col, row, trackBoard[row][col], hex.stepsFromStart, toVisit, trackBoard);
+		    	}
+	    	}
+		}
+	}
+	console.log("error: trying to check min moves to win for red and the game is over");
+	return 100000;
 }
 
-function getHeuristicScore(path, board)
+function insertHexagonRed(xPos, yPos, value, steps, toVisit, trackBoard)
 {
-	if (path == null)
+	if (value == -1 || value == 2) //red cannot move where blue has moved or on an illegal move
+		return;
+	else if (value == 0)
 	{
-		console.log("tried to calculate heuristic on end state board");
-		return 10000;
+		trackBoard[yPos][xPos] = -1
+		toVisit.push({xPos: xPos, yPos: yPos, stepsFromStart: steps + 1});
 	}
-	var doubleBridges = 0;
-	var moves = 0;
-	for (let i = 0; i < path.length; i++)
+	else if (value == 1)
 	{
-		var entry = path[i];
-		if (board[entry.y][entry.x] == 0)
-			moves++;
-    }
-    return moves;
+		trackBoard[yPos][xPos] = -1
+		toVisit.push({xPos: xPos, yPos: yPos, stepsFromStart: steps});
+	}
+}
+
+function insertHexagonBlue(xPos, yPos, value, steps, toVisit, trackBoard)
+{
+	if (value == -1 || value == 1) //red cannot move where blue has moved or on an illegal move
+		return;
+	else if (value == 0)
+	{
+		trackBoard[yPos][xPos] = -1
+		toVisit.push({xPos: xPos, yPos: yPos, stepsFromStart: steps + 1});
+	}
+	else if (value == 2)
+	{
+		trackBoard[yPos][xPos] = -1
+		toVisit.push({xPos: xPos, yPos: yPos, stepsFromStart: steps});
+	}
 }
