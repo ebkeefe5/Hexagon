ffaf725ae86f4b93b7451741cfccc9a4617e56d1
Eric Keefe <ekeefe@Unknown.domain>
2025-04-06 12:13:45 -0600

    remove ai

--- js/controller.js
+++ js/controller.js
@@ -1,113 +1,15 @@
 const newSingleGameButton = document.getElementById('newSingleGameButton');
-
-const selectBlueButton    = document.getElementById('selectBlueButton');
-const selectRedButton     = document.getElementById('selectRedButton');
-
 const restartGameButton   = document.getElementById('restartGame');
-
-const select1Btn    = document.getElementById('select1');
-const select2Btn    = document.getElementById('select2');
-const select3Btn  = document.getElementById('select3');
-
 const playerMoveLabel = document.getElementById('playerTurn');
 
 let board;
 let turn = 1;
-var AIPlayerNumber = 2;
-let playerNumber = 1;
 let difficulty = 1;
 let gameInProgress = false;
 let gameOver = false;
 
 newSingleGameButton.addEventListener('click', newSingleGame);
-selectBlueButton.addEventListener('click', selectBlue);
-selectRedButton.addEventListener('click', selectRed);
 restartGameButton.addEventListener('click', restartGame);
-select1Btn.addEventListener('click', select1);
-select2Btn.addEventListener('click', select2);
-select3Btn.addEventListener('click', select3);
-
-function selectRed()
-{
-  if (gameInProgress)
-  {
-    if (playerNumber != 1)
-      alert("Please restart game to change color");    
-    return;
-  }
-  selectBlueButton.style.background = '#A9A9A9';
-  turn = 1;
-  playerNumber = 1;
-  AIPlayerNumber = 2;
-  selectRedButton.style.background = colors[1];
-  playerTurn.innerHTML = 'Red\'s move!';
-}
-
-function selectBlue()
-{
-  if (gameInProgress)
-  {
-    if (playerNumber != 2)
-      alert("Please restart game to change color");
-    return;
-  }
-  selectRedButton.style.background = '#A9A9A9';
-  turn = 1;
-  playerNumber = 2;
-  AIPlayerNumber = 1;
-  selectBlueButton.style.background = colors[2];
-  if (difficulty == 1)
-    moveAI(board);
-  else if (difficulty == 2)
-    moveAILevel2(board);
-  else
-    moveAILevel3(board);
-  markGameInProgress();
-  updateTurn();
-  drawBoard();  
-}
-
-function select1()
-{
-  if (gameInProgress)
-  {
-    if (difficulty != 1)
-      alert ("Please restart game to change difficulty");
-    return;
-  }
-  difficulty = 1;
-  select1Btn.style.background = '#61666A';
-  select2Btn.style.background = '#A9A9A9';
-  select3Btn.style.background = '#A9A9A9';
-}
-
-function select2()
-{
-  if (gameInProgress)
-  {
-    if (difficulty != 2)
-      alert ("Please restart game to change difficulty");
-    return;
-  }
-  difficulty = 2;
-  select2Btn.style.background = '#61666A';
-  select1Btn.style.background = '#A9A9A9';
-  select3Btn.style.background = '#A9A9A9';
-}
-
-function select3()
-{
-  if (gameInProgress)
-  {
-    if (difficulty != 3)
-      alert ("Please restart game to change difficulty");
-    return;
-  }
-  difficulty = 3;
-  select3Btn.style.background = '#61666A';
-  select1Btn.style.background = '#A9A9A9';
-  select2Btn.style.background = '#A9A9A9';
-}
 
 function markGameInProgress()
 {
@@ -119,17 +21,17 @@ function markGameInProgress()
 function newSingleGame() {
   //the game hasn't been started until either the AI or player makes a move
   restartGame(); 
-  select1();
 }
 
 function restartGame()
 {
   gameInProgress = false; 
   gameOver = false;
+  turn = 1;
+  playerTurn.innerHTML = 'Red\'s move!';
   init();
   createBoard();
   drawBoard();  
-  selectRed();
 }
 
 function handleClickOnePlayer(row, col)
@@ -140,25 +42,12 @@ function handleClickOnePlayer(row, col)
     return;
   }
 
-  if (board[row][col] != 0 || turn != playerNumber)
+  if (board[row][col] != 0)
     return;
 
-  markGameInProgress();
+  gameInProgress = true;
 
-  board[row][col] = playerNumber;
-  updateTurn();
-
-  if (turn == 2 && checkRedWin(board))
-    return;
-  if (turn == 1 && checkBlueWin(board))
-    return;
-
-  if (difficulty == 1)
-    moveAI(board);
-  else if (difficulty == 2)
-    moveAILevel2(board);
-  else
-     moveAILevel3(board);
+  board[row][col] = turn;
   updateTurn();
 
   if (turn == 2 && checkRedWin(board))
